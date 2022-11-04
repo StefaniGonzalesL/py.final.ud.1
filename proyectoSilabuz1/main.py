e8721fd1cc3865666761e0955dc6c938c251e6e9
Moises Atay <macmoisesataypoma@gmail.com>
2022-11-04 12:24:20 -0500

    comentario nuevo

--- proyectoSilabuz1/main.py
+++
@@ -1,8 +0,0 @@
-class Libro:
-    def __init__(self, id: int, titulo: str, genero: str, ISBN: str, editorial: str, autores: list):
-        self.id = id
-        self.titulo = titulo
-        self.genero = genero
-        self.ISBN = ISBN
-        self.editorial = editorial
-        self.autores = autores
