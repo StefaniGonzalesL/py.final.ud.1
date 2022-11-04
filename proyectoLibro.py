df6f55e1a008820713713e909feeaaa21f2eee2f
Moises Atay <macmoisesataypoma@gmail.com>
2022-11-04 13:13:39 -0500

    clase libro

--- proyectoLibro.py
+++ proyectoLibro.py
@@ -1,2 +1,17 @@
 class Libro:
     pass
+
+
+print("Opciones: ")
+print("Escriba un numero de 1 a 10: ")
+print("1: Leer archivo de disco duro (.txt o csv) que cargue 3 libros")
+print("2: Listar libros")
+print("3: Agregar libro")
+print("4: Eliminar libro")
+print("5: Buscar libro por ISBN o por título. Se debe sugerir las opciones y listar el resultado")
+print("6: Ordenar libros por título")
+print("7: Buscar libros por autor, editorial o género. Se deben sugerir las opciones y listar los resultados")
+print("8: Buscar libros por número de autores. Se debe ingresar un número por ejemplo 2 (hace referencia a dos autores) y se deben listar todos los libros que contengan 2 autores")
+print("9: Editar o actualizar datos de un libro (título, género, ISBN, editorial y autores)")
+print("10: Guardar libros en archivo de disco duro (.txt o csv)")
+
