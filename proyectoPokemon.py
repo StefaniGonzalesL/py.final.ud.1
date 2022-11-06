8949eb317ddfa2a927eba3bc47632dbb7b1b983a
Moises Atay <macmoisesataypoma@gmail.com>
2022-11-06 18:08:04 -0500

    obteniendo URLs de las opciones

--- proyectoPokemon.py
+++ proyectoPokemon.py
@@ -1,15 +1,28 @@
 import requests
+import os
 
-url="https://pokeapi.co/api/v2/"
-peticion=requests.get(url)
-datos_json=peticion.json()
-for k,v in datos_json.items():
-    print(f"{k}: {v}")
+url_general="https://pokeapi.co/api/v2/"
+
+def leer_json(url:str)->dict:
+    peticion=requests.get(url)
+    return peticion.json()
+
+
+datos_json=leer_json(url_general)
+
+url_generacion=datos_json["generation"]
+url_forma=datos_json["pokemon-shape"]
+url_habilidad=datos_json["ability"]
+url_habitat=datos_json["pokemon-habitat"]
+url_tipo=datos_json["type"]
+
+
+def listarPokemonGeneracion(urlGeneracion, numeroGeneracion):
+    pass
+
 
 def main():
     enEjecucion = True
-    file = lista_pokemon
-    lista_pokemon = leer_archivos(file)
 
     while (enEjecucion):
         input("hola, presione cuaquier tecla para continuar")
@@ -20,34 +33,47 @@ def main():
         print("3: Listar pokemons por habilidad")
         print("4: Listar pokemons por habitat")
         print("5: Listar pokemons por tipo")
+        print("6: Salir")
 
-        opcion = int(input("Elige una opción (de 1 a 5): "))
+        opcion = int(input("Elige una opción (de 1 a 6): "))
         
         if opcion == 1:
             os.system("cls")
             opciones_busqueda = int(input("Ingresa una generación del 1-8: \n"))
 
             while opciones_busqueda < 1 or opciones_busqueda > 8:   
-                opciones_busqueda = int(input("Opcion Incorrecta, ingrese nuevamente una opcion valida"
-                "\n)
+                opciones_busqueda = int(input("Opcion Incorrecta, ingrese nuevamente una opcion valida\n"))
         
         elif opcion == 2:
-            os.system("cls")
-            opciones_busqueda = int(input("Ingresa una forma: \n")) #llamar a la lista de formas
-            print(opciones_busqueda)
+           os.system("cls")
+           opciones_busqueda = int(input("Ingresa una forma: \n")) #llamar a la lista de formas
+           print(opciones_busqueda)
 
         
         elif opcion ==3:
-             os.system("cls")
-             opciones_busqueda = int(input("Ingresa una habilidad: \n"))
+            os.system("cls")
+            opciones_busqueda = int(input("Ingresa una habilidad: \n"))
             print(opciones_busqueda)
 
         elif opcion ==4:
-             os.system("cls")
-             opciones_busqueda = int(input("Ingresa una habitat: \n"))
+            os.system("cls")
+            opciones_busqueda = int(input("Ingresa una habitat: \n"))
             print(opciones_busqueda)
 
-         elif opcion ==5:
-             os.system("cls")
-             opciones_busqueda = int(input("Ingresa un tipo: \n"))
+        elif opcion ==5:
+            os.system("cls")
+            opciones_busqueda = int(input("Ingresa un tipo: \n"))
             print(opciones_busqueda)
+
+        elif opcion==6:
+            os.system("cls")
+            enEjecucion=False
+        else:
+            os.system("cls")
+            print("Opción inválida, ingrese nuevamnete una opcion(del 1 a 6)")
+
+
+if __name__ == "__main__":
+    main()
+
+
